502ec84ea6ecac6cd8e86fb70e8696e4ea4affed
Sebastien SERRE <sebastien.serre@univ-nantes.fr>
2025-01-03 19:49:43 +0100

    add some log info

--- bin/main.ml
+++ bin/main.ml
@@ -1,22 +1,56 @@
-
 let app_name = "webapptpl"
 let app_version = "1.0"
 
+let count_success = ref 0
+let count_failed = ref 0
 
-let custom_middleware next request =
+
+(** asynchronous middleware example *)
+let lwt_count_middleware inner request =
+  try%lwt
+    let%lwt response = inner request in
+    count_success := !count_success + 1;
+    Lwt.return response
+  with exn ->
+    count_failed := !count_failed + 1;
+    raise exn
+
+
+(** synchronous middleware example *)
+let sync_print_middleware inner request =
   print_endline "request!";
-  next request
+  inner request
+
 
 let run listen secure =
-    Dream.run ~tls:secure ~port:listen
-      @@ Dream.logger
-      @@ custom_middleware
-      @@ Dream.router [
-        Dream.get "/" (fun _ -> Dream.html "hello dream!");
-        Dream.get "/echo/:word" (fun request -> 
-          Dream.html (Dream.param request "word"))
+  let open Lwt in
+  Dream.run 
+    ~tls:secure 
+    ~port:listen
+    ~error_handler:Dream.debug_error_handler
+    @@ Dream.logger
+    @@ sync_print_middleware
+    @@ lwt_count_middleware
+    @@ Dream.router [
+      Dream.get "/fail" (fun _ -> raise (Failure "custom failure"));
+      Dream.get "/count" (fun _ -> 
+        Printf.sprintf "ok:%i err:%i" !count_success !count_failed 
+        |> Dream.html);
+      Dream.get "/" (fun _ -> Dream.html "hello dream!");
+      Dream.get "/echo/:word" (fun request -> 
+        Dream.html (Dream.param request "word"));
+      Dream.post "/print_request" (fun request ->
+        let%lwt body = Dream.body request in
+        Dream.respond
+          ~headers:["Content-Type", "application/octet-stream"]
+          body);
+      Dream.post "/print_request2" (fun request ->
+        Dream.body request 
+        >>=
+        Dream.respond
+          ~headers:["Content-Type", "application/octet-stream"]
+          );
       ]
-
 
 
 (* 
@@ -32,7 +66,8 @@ let start cfg_file =
       and secure = 
           Toml.Types.Table.find_opt (Toml.Min.key "secure") vals
         |> function | Some Toml.Types.TBool v -> v | _ -> false in
-      run listen secure
+
+      run listen secure 
 
 module App = struct
   open Cmdliner
--- test/req.sh
+++ test/req.sh
@@ -2,6 +2,15 @@
 
 
 
-curl http://localhost:8080
+#curl http://localhost:8080
 
-curl http://localhost:8080/echo/helllksdjfo
+#curl http://localhost:8080/echo/helllksdjfo
+
+#curl http://localhost:8080/fail
+#curl http://localhost:8080/fail
+
+#curl http://localhost:8080/count
+#curl http://localhost:8080/count
+
+curl -X POST http://localhost:8080/print_request --data "dsqkdfj"
+curl -X POST http://localhost:8080/print_request2 --data "aaaaaaaaa"
